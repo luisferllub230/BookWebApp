b899d5c878bdfda496f0e04d4b989ba3d5e1c8d4
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-07-26 14:59:28 -0400

    type: added edit and delete editorials

--- src/controller/admin.js
+++ src/controller/admin.js
@@ -26,18 +26,16 @@ const getAuthor = (req, res, next) => {
 const getEditorials = (req, res, next) => {
 
     em.findAll({
+
         attributes: {
             include: [[sequelize.fn('COUNT', sequelize.col('books.editorialId')), 'booksCount']]
         },
         include: [{
             model: bm, attributes: []
         }]
+
     }).then(e=>{
-    
         const editorials = e.map(e=>e.dataValues);  
-            
-        console.log(editorials);
-    
         res.render('./admin/editorialsTools',{
             title: 'Editorials',
             activeEditorials: true,
@@ -45,7 +43,6 @@ const getEditorials = (req, res, next) => {
         });
     }).catch(err => console.log(err));
 }
-
 const posEditorials = (req, res, next) => {
     em.create({
         EditorialName: req.body.editorialN,
@@ -56,4 +53,58 @@ const posEditorials = (req, res, next) => {
     }).catch(err => console.log(err));
 }
 
-export {getBooks, getCategories, getAuthor, getEditorials, posEditorials};
+
+//*********************edit*****************/
+const getEdit = (req, res, next) => {
+    const type = req.params.type;
+    const id = req.params.id;
+
+    if(type === 'editorial'){
+        em.findOne({ where: {id: id}}).then(e=>{
+            const editorial = e.dataValues;
+            res.render('edit',{
+                title: 'Edit Editorial',
+                activeEditorial: true,
+                editorial
+            })
+        }).catch(err => console.log(err));
+    }
+}
+const posEdit = (req, res, next) => {
+    const type = req.params.type;
+
+    if(type === 'editorial'){
+        em.update({
+            EditorialName: req.body.editorialN,
+            EditorialPhone: req.body.editorialP,
+            EditorialCountry: req.body.editorialC,
+        },
+        {where: {id: req.body.id}
+    }).then(()=>res.status(200).redirect('/admin/editorials')).catch(err => console.log(err));
+    }
+}
+
+/**********************delete**************************/
+const getDelete = (req, res, next) => {
+    const type = req.params.type;
+    const id = req.params.id;
+
+    if(type === 'editorial'){
+        em.findOne({ where: {id: id}}).then(e=>{
+            const editorial = e.dataValues;
+            res.render('delete',{
+                title: 'Delete Editorial',
+                activeEditorial: true,
+                editorial
+            })
+        }).catch(err => console.log(err));
+    }
+}
+const posDelete = (req, res, next) => {
+    const type = req.params.type;
+    if(type === 'editorial'){
+        em.destroy({where: {id: req.body.id}}).then(()=>res.status(200).redirect('/admin/editorials')).catch(err => console.log(err));
+    }
+}
+
+export {getBooks, getCategories, getAuthor, getEditorials, posEditorials, getEdit, posEdit, getDelete, posDelete};
--- src/routers/adminRouts.js
+++ src/routers/adminRouts.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import {getBooks, getCategories, getAuthor, getEditorials, posEditorials} from '../controller/admin.js';
+import {getBooks, getCategories, getAuthor, getEditorials, posEditorials, posEdit, getEdit, getDelete, posDelete} from '../controller/admin.js';
 
 const adminRoutes = express.Router();
 
 adminRoutes.get('/books', getBooks);
 adminRoutes.get('/categoric', getCategories);
 adminRoutes.get('/author', getAuthor);
+
 adminRoutes.get('/editorials', getEditorials);
 adminRoutes.post('/editorials', posEditorials);
+
+adminRoutes.post('/edit/:type', posEdit);
+adminRoutes.get('/edit/:type/:id', getEdit);
+
+adminRoutes.post('/delete/:type', posDelete);
+adminRoutes.get('/delete/:type/:id', getDelete);
 
 export default adminRoutes;
